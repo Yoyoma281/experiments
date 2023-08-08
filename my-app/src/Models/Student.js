eaa02061d38950ab6b63ade71c3d4a51ec47b3e2
Yoyoma281 <117493856+Yoyoma281@users.noreply.github.com>
2023-08-08 17:53:35 +0300

    Delete my-app directory

--- my-app/src/Models/Student.js
+++
@@ -1,9 +0,0 @@
-export default class StudentModel{
-    constructor(firstname = "", lastname = "", age = 0, gender = "", image = ""){
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.age = age;
-        this.gender = gender;
-        this.image = image; 
-    }
-}
