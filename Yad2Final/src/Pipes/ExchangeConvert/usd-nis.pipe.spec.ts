772cd2be180b153d1b64f06be47973ed8810b5a6
Yoyoma281 <117493856+Yoyoma281@users.noreply.github.com>
2023-08-08 17:48:05 +0300

    Delete Yad2Final directory

--- Yad2Final/src/Pipes/ExchangeConvert/usd-nis.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { USDNISPipe } from './usd-nis.pipe';
-
-describe('USDNISPipe', () => {
-  it('create an instance', () => {
-    const pipe = new USDNISPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
